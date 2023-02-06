1bd3d4fe95c67367301614eeeee55940aeb8992e
omrazCZ <37776141+omrazCZ@users.noreply.github.com>
2023-02-06 14:33:18 +0100

    Update number of workers

--- experiments/autocaching/manage_cluster.sh
+++ experiments/autocaching/manage_cluster.sh
@@ -39,8 +39,8 @@ echo_failure() {
 
 programname=$0
 gluster_nodes=1
-num_kubernetes_nodes=1
-num_tfdata_workers=1
+num_kubernetes_nodes=10
+num_tfdata_workers=10
 nethz="otmraz"
 region="europe-west4"
 zone="europe-west4-a"
